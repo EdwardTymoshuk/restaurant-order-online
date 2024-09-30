3032f02ee53fb194b0d7efd6a51f5feb87e3b43f
Edward <eduard.tymoshuk@gmail.com>
2024-09-30 07:45:47 +0200

    feat: Update image display in menu item editing and add loader button
    - Changed the way images are displayed during menu item editing and creation
    - Added a button with a loader for better UX during operations

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['spoko-sopot.r2.cloudflarestorage.com'],
+		domains: ['spoko-sopot.r2.cloudflarestorage.com', 'pub-6b43bbf3eb884034863e85b0eefe37a8.r2.dev'],
 	},
 }
 
