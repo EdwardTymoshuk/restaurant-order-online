0486a9f6c5fe3d25d4c7ad4cf807f00898ad989e
Edward <eduard.tymoshuk@gmail.com>
2024-10-03 21:03:12 +0200

    feat: Add close button and outside click functionality for sidebar menu
    - Added a close button to the sidebar menu
    - Enabled closing the menu when clicking outside of it
    - Adjusted padding for the menu page for better layout

--- src/app/admin-panel/components/sidebar.tsx
+++ src/app/admin-panel/components/sidebar.tsx
@@ -1,18 +1,28 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-
+import { MdClose } from 'react-icons/md' // Додаємо іконку закриття
 
 interface SidebarProps {
 	children: React.ReactNode
 	className?: string
+	onClose?: () => void // Функція для закриття меню
 }
 
-export const Sidebar = ({ children, className }: SidebarProps) => {
+export const Sidebar = ({ children, className, onClose }: SidebarProps) => {
 	return (
-		<nav className={cn("min-w-64 w-fit bg-secondary shadow-primary shadow-sm h-full", className)}>
-			<div className="p-6">
+		<nav id="sidebar" className={cn("min-w-64 w-fit bg-secondary shadow-primary shadow-sm h-full", className)}>
+			<div className="p-6 flex items-center justify-between">
+				{/* Заголовок Admin Panel */}
 				<h1 className="text-xl font-bold text-text-primary">Admin Panel</h1>
+
+				{/* Кнопка для закриття, яка буде поруч з заголовком */}
+				<button
+					onClick={onClose} // Виклик функції для закриття
+					className="lg:hidden text-text-primary hover:text-primary transition-all"
+				>
+					<MdClose size={28} />
+				</button>
 			</div>
 			<ul className="pt-8">
 				{children}
