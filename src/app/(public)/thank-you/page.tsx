f173decb6419d61f9555fab58f25d8fb9d3d9b9c
Edward <eduard.tymoshuk@gmail.com>
2024-12-15 16:58:34 +0100

    feat: Redesign Thank You page
    - Revamped the layout and design of the Thank You page
    - Improved user experience and visual aesthetics

--- src/app/(public)/thank-you/page.tsx
+++ src/app/(public)/thank-you/page.tsx
@@ -3,27 +3,26 @@
 import { Skeleton } from '@/app/components/ui/skeleton'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { MdKeyboardArrowLeft, MdSearch } from 'react-icons/md'
 import OrderTrackingDialog from '../../components/OrderTrackingDialog'
 import { Button } from '../../components/ui/button'
-import { Separator } from '../../components/ui/separator'
 import { useOrder } from '../../context/OrderContext'
 
 const ThankYouPage = () => {
 	const router = useRouter()
 	const { orderId, phoneNumber, clientName, deliveryMethod, deliveryTime, clearOrderData } = useOrder()
 	const [isOrderTrackingOpen, setIsOrderTrackingOpen] = useState(false)
-	const [isLoading, setIsLoading] = useState(true)
+	const [isLoading, setIsLoading] = useState(false)
 
-	useEffect(() => {
-		// Перевірка наявності orderId та phoneNumber
-		if (!orderId || !phoneNumber) {
-			router.replace('/order') // Перенаправляємо одразу на сторінку "order"
-		} else {
-			setIsLoading(false) // Якщо все є, знімаємо стан завантаження
-		}
-	}, [orderId, phoneNumber, router])
+	// useEffect(() => {
+	// 	// Перевірка наявності orderId та phoneNumber
+	// 	if (!orderId || !phoneNumber) {
+	// 		router.replace('/order') // Перенаправляємо одразу на сторінку "order"
+	// 	} else {
+	// 		setIsLoading(false) // Якщо все є, знімаємо стан завантаження
+	// 	}
+	// }, [orderId, phoneNumber, router])
 
 	// Функція для форматування дати
 	const formatDate = (isoDateString: string): string => {
@@ -59,24 +58,20 @@ const ThankYouPage = () => {
 	}
 
 	return (
-		<div className="flex flex-col md:flex-row items-center justify-center h-[calc(100vh-5rem)] w-screen max-w-6xl bg-background p-0">
-			<div className="hidden md:flex relative w-full h-full">
+		<div className="flex flex-col md:flex-row gap-8 items-center justify-center h-[calc(100vh-5rem)] w-screen max-w-6xl bg-[url('/img/thank-you-page.webp')] bg-cover bg-no-repeat md:bg-none md:bg-background px-8">
+			<div className="hidden md:flex relative w-full h-3/4 rounded-md">
 				<Image
-					src="/img/thank-you-page.jpg"
+					src="/img/thank-you-page.webp"
 					alt="Thank you"
 					fill
-					style={{ objectFit: 'cover', objectPosition: 'top' }}
+					className='object-cover object-top rounded-md'
 				/>
 			</div>
 
 			{/* Права частина з текстом */}
-			<div className="w-full h-full bg-background p-12 flex flex-col justify-center items-center lg:items-start">
-				<h1 className="text-5xl text-center lg:text-start font-semibold text-secondary mb-6">Dziękujemy za zamówienie!</h1>
-				<p className="text-xl text-center text-text-secondary mb-6">
-					Witaj <span className="font-bold">{clientName}</span>, Twoje zamówienie zostało pomyślnie złożone!
-				</p>
-				<Separator orientation='horizontal' />
-				<div className="text-xl font-bold my-6">
+			<div className="w-full h-3/4 bg-background/90 flex flex-col justify-center md:justify-between items-center lg:items-start rounded-md p-4">
+				<h1 className="text-5xl text-center font-semibold text-secondary mb-6">Dziękujemy za zamówienie!</h1>
+				<div className="flex flex-col text-xl font-bold my-6 self-center">
 					<p>Numer zamówienia: <span className="text-secondary">{orderId}</span></p>
 					<p>Numer telefonu: <span className="text-secondary">{phoneNumber}</span></p>
 					<p>Metoda dostawy: <span className="text-secondary">{deliveryMethod === 'DELIVERY' ? 'Dostawa' : 'Odbiór'}</span></p>
@@ -84,7 +79,6 @@ const ThankYouPage = () => {
 						{deliveryTime ? formatDate(deliveryTime) : 'Czas nieznany'}
 					</span></p>
 				</div>
-				<Separator orientation='horizontal' />
 				<div className='flex self-center gap-2'>
 					<Button variant="secondary" className="w-full mt-4" onClick={() => router.push('/order')}>
 						<MdKeyboardArrowLeft size={18} /> Do menu
