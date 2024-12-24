eb84a300ffb1a9bc93d73ad58a6be88e343cc876
Edward <eduard.tymoshuk@gmail.com>
2024-12-24 13:55:36 +0100

    fix: Resolve issues on orders page and update middleware for login redirection
    - Fixed various issues on the orders page for better functionality
    - Updated middleware to correctly redirect users to the login page

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -1,35 +1,6 @@
 // src/app/admin-panel/page.tsx
-'use client'
-
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { Suspense, useEffect } from 'react'
-import { Skeleton } from '../components/ui/skeleton'
 import AdminPanelContent from './components/AdminPanelContent'
 
 export default function AdminPanel() {
-
-	const { data: session, status } = useSession()
-	const router = useRouter()
-
-	useEffect(() => {
-		if (status === 'loading') return
-		if (status === 'unauthenticated') {
-			router.push('/admin-panel/auth/login')
-		}
-	}, [status, session, router])
-
-	return (
-		<Suspense fallback={
-			<div className='flex flex-col w-full p-8'>
-				<Skeleton className='h-8 w-full mb-4' />
-				<Skeleton className='h-8 w-full mb-4' />
-				<Skeleton className='h-8 w-full mb-4' />
-				<Skeleton className='h-8 w-full mb-4' />
-				<Skeleton className='h-8 w-full mb-4' />
-			</div>
-		}>
-			<AdminPanelContent />
-		</Suspense>
-	)
+	return <AdminPanelContent />
 }
