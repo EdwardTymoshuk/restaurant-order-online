9cf7dbafd3eaff1ca51304c1de3428bcf4a8f8a8
Edward <eduard.tymoshuk@gmail.com>
2026-08-16 17:02:47 +0200

    Externalize GeoIP package in Next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
-		serverComponentsExternalPackages: ['pdf-parse', 'pdfjs-dist'],
+		serverComponentsExternalPackages: ['pdf-parse', 'pdfjs-dist', 'geoip-lite'],
 	},
 	images: {
 		domains: ['spoko-sopot.r2.cloudflarestorage.com', 'pub-6b43bbf3eb884034863e85b0eefe37a8.r2.dev'],
